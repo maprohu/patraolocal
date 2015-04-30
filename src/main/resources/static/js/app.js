780c9428edcaf78b780dbc95ae44fc37f1a32374
maprohu <maprohu@gmail.com>
2015-04-30 17:33:43 +0100

    working on choice with cross references all the rest seems to work for now

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,6 +1,39 @@
 var app = angular.module('mfw-app', [
-	'ui.router'                                    
-]).config(function($stateProvider, $urlRouterProvider) {
+                                     
+	'ui.router'                      
+	
+]).directive('plComplete', function($compile) {
+	return {
+		restrict: 'E',
+		link: function(scope, element, attrs) {
+			scope.$watch(function(scope) {
+				return scope.$eval(attrs.data);
+			}, function(value) {
+				value = value.replace(/{{/g, '<pl-complete-field handler="handler" model="model" solution="\'').replace(/}}/g, '\'"></pl-complete-field>')
+				element.html('<div>'+value+'</div>');
+				$compile(element.contents())(scope);
+			})
+		}
+	};
+	
+}).directive('plCompleteField', function() {
+	
+	return {
+		restrict: 'E',
+		scope: {
+		      solution: '=',
+		      handler: '=',
+		      model: '='
+		},
+		templateUrl: 'partials/exercise/complete/field.html',
+		link: function(scope, element, attr) {
+			scope.$on("plInitialize", function() {
+				scope.value = null;
+			});
+		}
+	};
+
+}).config(function($stateProvider, $urlRouterProvider) {
 	
 	$stateProvider
 	.state('practice', {
@@ -10,23 +43,159 @@ var app = angular.module('mfw-app', [
 
 }).controller('MainCtrl', function($scope, $http, $resource, $location, $state, CONFIG) {
 
+	function extend(base, sub) {
+		var origProto = sub.prototype;
+		sub.prototype = Object.create(base.prototype);
+		for ( var key in origProto) {
+			sub.prototype[key] = origProto[key];
+		}
+		sub.prototype.constructor = sub;
+		Object.defineProperty(sub.prototype, 'constructor', {
+			enumerable : false,
+			value : sub
+		});
+	}
+	
+	function random(i) {
+		return Math.floor(Math.random() * i);
+	}
+	
+	function shuffle(o){
+	    for(var j, x, i = o.length; i; j = random(i), x = o[--i], o[i] = o[j], o[j] = x);
+	    return o;
+	}	
+	
+	$scope.model = {};
+	
+	function Handler(topic, exercise) {
+		this.topic = topic;
+		this.exercise = exercise;
+	}
+	
+	Handler.prototype = {
+		getTemplateName: function() {
+			return this.exercise.type;
+		},
+		getTemplate: function() {
+			return 'partials/exercise/' + this.getTemplateName() + '.html'
+		},
+		initialize: function() {
+			
+		},
+		check: function() {
+			$scope.model.showSolution = true;
+		},
+		stripDown: function(value) {
+			return (value||'').trim().toLowerCase().replace(/\s+/g, ' ');
+		},
+		isCorrect: function(value, solution) {
+			return this.stripDown(value) === this.stripDown(solution);
+		}
+		
+	};
+	
+	function ChoiceHandler(topic, exercise) {
+		Handler.call(this, topic, exercise);
+	}
+	ChoiceHandler.prototype = {
+		initialize: function() {
+			var indexes = [];
+			for (var i = 0 ; i < this.exercise.alternatives.length ; i++) {
+				indexes.push(i);
+			}
+			shuffle(indexes);
+			var items = shuffle(this.exercise.alternatives.slice());
+			var correct = random(items.length + 1);
+			items.splice(correct, 0, this.exercise.solution);
+			
+			$scope.model.elements = [this.exercise.solution].concat(this.exercise.alternatives);
+			$scope.model.indexes = indexes;
+			$scope.model.correct = correct;
+		},
+		forward: function(index) {
+			return $scope.model.indexes[index];
+		},
+		reverse: function(index) {
+			return $scope.model.indexes.indexOf(index); 
+		},
+		parse: function(text) {
+			
+			
+		},
+		select: function(selection) {
+			$scope.model.selection = selection;
+			this.check();
+		},
+		showDefault: function(index) {
+			return !this.showCorrect(index) && !this.showIncorrect(index);
+		},
+		showCorrect: function(index) {
+			return $scope.model.showSolution && $scope.model.correct == index;
+		},
+		showIncorrect: function(index) {
+			return $scope.model.showSolution && $scope.model.correct != index && $scope.model.selection == index;
+		}
+	};
+	extend(Handler, ChoiceHandler);
+	
+	function CompleteHandler(topic, exercise) {
+		Handler.call(this, topic, exercise);
+	}
+	CompleteHandler.prototype = {
+		isStrict: function() {
+			return true;
+		}
+	};
+	extend(Handler, CompleteHandler);
+	
+	function CompleteTextHandler(topic, exercise) {
+		CompleteHandler.call(this, topic, exercise);
+	}
+	CompleteTextHandler.prototype = {
+		isStrict: function() {
+			return false;
+		},
+		getTemplateName: function() {
+			return 'complete';
+		}
+	};
+	extend(CompleteHandler, CompleteTextHandler);
+	
+	
+	function ItemsHandler(topic, exercise) {
+		Handler.call(this, topic, exercise);
+	}
+	ItemsHandler.prototype = {
+		initialize: function() {
+			$scope.model.values = [];
+		},
+		isIncluded: function(value, elements) {
+			var that = this;
+			return (elements||[]).some(function(element) {
+				return that.isCorrect(value, element);
+			});
+		}
+	};
+	extend(Handler, ItemsHandler);
+
+	
 	$scope.exercises = [];
-	$scope.currentExercise = null;
+	$scope.handler = null;
 	
 	$scope.nextExercise = function() {
-		$scope.currentExercise = $scope.exercises[Math.floor(Math.random()*$scope.exercises.length)];
+		$scope.model = {};
+		$scope.handler = $scope.exercises[random($scope.exercises.length)];
+		$scope.handler.initialize();
+		$scope.$broadcast("plInitialize");
 	}
 
-	var exercise = function() {
-		return $scope.currentExercise.exercise;
-	}
 	var exerciseHandlers = {
-			text: {
-				template: function() {
-					return 'partials/exercise/' + exercise().type + '.html';
-				}
-				
-			}
+		text: Handler,
+		phrase: Handler,
+		choice: ChoiceHandler,
+		complete: CompleteHandler,
+		complete_text: CompleteTextHandler,
+		items: ItemsHandler,
 	};
 	
 	
@@ -35,11 +204,7 @@ var app = angular.module('mfw-app', [
 		
 		$scope.exercises = [].concat.apply([], data.map(function(topic) {
 			return topic.exercises.map(function(exercise) {
-				return {
-					topic: topic,
-					exercise: exercise,
-					handler: exerciseHandlers[exercise.type] 
-				}
+				return new exerciseHandlers[exercise.type](topic, exercise);
 			});
 		}));
 		
